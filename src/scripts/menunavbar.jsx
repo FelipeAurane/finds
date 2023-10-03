4cbcb5c4867dbc69bcc8ded3a560d3e4672bb267
Felipe Aurane <felipeaurane@gmail.com>
2023-10-02 23:53:07 -0300

    commit 7

--- src/scripts/menunavbar.jsx
+++ src/scripts/menunavbar.jsx
@@ -7,5 +7,12 @@ function activeLink() {
 
 list.forEach((item) => {
   item.addEventListener('click', activeLink);
-  item.addEventListener('touchstart', activeLink); // Adicione o evento de toque (touchstart)
+
+  // Adicione os eventos de toque para dispositivos móveis
+  item.addEventListener('touchstart', function (e) {
+    // Previna o comportamento padrão do toque (evita que a tela role, etc.)
+    e.preventDefault();
+    // Execute a função activeLink
+    activeLink.call(this);
+  });
 });
